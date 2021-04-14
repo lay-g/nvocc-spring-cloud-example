36b5f8ad12b336f11a6c726356373825cc0b58fa
Lay <guanlei@geeweb.cn>
2021-04-14 17:11:05 +0800

    add gateway

---
+++ cloud-gateway-example/src/main/java/com/nvocc/example/gateway/CloudGatewayExampleApplication.java
@@ -0,0 +1,13 @@
+package com.nvocc.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatewayExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayExampleApplication.class, args);
+    }
+
+}
---
+++ cloud-gateway-example/src/test/java/com/nvocc/example/gateway/CloudGatewayExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvocc.example.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
